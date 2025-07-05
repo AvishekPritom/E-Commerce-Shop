b10798ff5de705ef313423236049820b3730334b
Avishek Chowdhury <chyavishek2011@gmail.com>
2025-07-06 00:25:38 +0600

    Adding Toastify Alerts

--- Frontend/src/components/cart/CartPage.jsx
+++ Frontend/src/components/cart/CartPage.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import CartItem from "./CartItem";
 import CartSummary from "./CartSummary";
+import api from "../../api";
 
 const CartPage = () => {
+  const cart_code = localStorage.getItem("cart_code");
+  const [cart_items, setCartItems] = useState([]);
+  const [cartTotal, setcartTotal] = useState(0.0);
+  const tax = 4.0;
+
+  useEffect(function () {
+    api
+      .get(`get_cart?cart_code=${cart_code}`)
+      .then((res) => {
+        console.log(res.data);
+        setCartItems(res.data.items);
+        setcartTotal(res.data.total);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div
       className="container my-3 py-3"
@@ -10,10 +31,13 @@ const CartPage = () => {
       <h5 className="mb-4">Shopping Cart</h5>
       <div className="row">
         <div className="col-md-8">
-          {/* Cart Items will be rendered here */}
+          {cart_items.map((item) => (
+            <cartItem key={item.id} item={item} />
+          ))}
+
           <CartItem />
         </div>
-        <CartSummary />
+        <CartSummary cartTotal={cartTotal} tax={tax} />
       </div>
     </div>
   );
